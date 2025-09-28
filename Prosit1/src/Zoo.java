3b794f04ee1e3edf8b758fb716c804ae263e777f
Aziz Ben Lamine <azizbenlamine@gmail.com>
2025-09-28 21:59:22 +0100

    Complete Prosit 3 ✅

--- Prosit1/src/Zoo.java
+++ Prosit1/src/Zoo.java
@@ -2,27 +2,88 @@ public class Zoo {
     Animal[] animals = new Animal[25];
     String name;
     String city;
-    int nbrCages;
+    private static final int NBR_CAGES = 25;
+    int animalCount = 0;
 
     Zoo(){}
     //Instruction 6
-    Zoo(String name, String city, int nbrCages) {
+    Zoo(String name, String city) {
         this.name = name;
         this.city = city;
-        this.nbrCages = nbrCages;
     }
 
     //Instruction 8
     void displayZoo() {
         System.out.println("Nom: " + this.name);
         System.out.println("Ville: " + this.city);
-        System.out.println("NB cages: " + this.nbrCages);
+        System.out.println("NB cages: " + NBR_CAGES);
     }
 
     //Instruction 9
     @Override
     public String toString() {
-        return this.name + " " + this.city + " " + this.nbrCages;
+        return this.name + " " + this.city + " " + NBR_CAGES;
+    }
+
+    //Instruction 12
+    boolean addAnimal(Animal animal) {
+
+        if (searchAnimal(animal) != -1) {
+            return false;
+        }
+
+        if (animalCount < animals.length) {
+            animals[animalCount] = animal;
+            animalCount++;
+            return true;
+        }
+        return false;
+    }
+
+    void displayAnimals() {
+        for (int i = 0; i < animalCount; i++) {
+            System.out.println(animals[i]);
+        }
+    }
+
+    //Instruction 11
+    int searchAnimal(Animal animal) {
+        for (int i = 0; i < animalCount; i++) {
+            if (animals[i] != null && animals[i].name.equals(animal.name)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    //Instruction 13
+    boolean removeAnimal(Animal animal) {
+        int index = searchAnimal(animal);
+        if (index == -1) {
+            return false;
+        }
+
+        for (int i = index; i < animalCount - 1; i++) {
+            animals[i] = animals[i + 1];
+        }
+        animals[animalCount - 1] = null;
+        animalCount--;
+        return true;
+    }
+
+    //Instruction 15
+    boolean isZooFull(){
+        return animalCount >= NBR_CAGES;
+    }
+
+    Zoo comparerZoo(Zoo z1, Zoo z2) {
+        if (z1.animalCount > z2.animalCount) {
+            return z1;
+        } else if (z2.animalCount > z1.animalCount) {
+            return z2;
+        } else {
+            return null;
+        }
     }
 
 }
--- Prosit1/src/ZooManagement.java
+++ Prosit1/src/ZooManagement.java
@@ -3,33 +3,16 @@ import java.util.Scanner;
 public class ZooManagement {
     public static void main(String[] args) {
 
-        // Instruction 5
-        /*
-        Animal lion = new Animal();
-        lion.family = "Felidae";
-        lion.name = "Lion";
-        lion.age = 5;
-        lion.isMammal = true;
+        //Instruction 10
+        Zoo myZoo = new Zoo("Friguia", "Tunis");
 
+        for (int i = 1; i <= 23; i++) {
+            Animal a = new Animal("Family" + i, "Animal" + i, i, true);
+            myZoo.addAnimal(a);
+        }
+        Animal b = new Animal("Lion", "Jean", 5, true);
+        myZoo.addAnimal(b);
 
-        Zoo myZoo = new Zoo();
-        myZoo.name = "Safari Park";
-        myZoo.city = "Tunis";
-        myZoo.nbrCages = 10;
-        myZoo.animals[0] = lion;
-        */
-        // Instruction 7
-        Zoo myZoo = new Zoo("MyZoo", "Tunis", 10);
-        Animal animal1 = new Animal("Felidae", "Lion", 5, true);
-        Animal animal2 = new Animal("Canidae", "Wolf", 3, true);
-        Animal animal3 = new Animal("Ursidae", "Bear", 7, true);
-        myZoo.animals[0] = animal1;
-        myZoo.animals[1] = animal2;
-        myZoo.animals[2] = animal3;
-
-        // Instruction 8
-        myZoo.displayZoo();
-        System.out.println(myZoo);
-        System.out.println(animal2.toString());
+        System.out.println(myZoo.searchAnimal(b));
     }
 }
